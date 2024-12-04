b354c7ca5ba5fb3181d2013af6ecb15f74075ccc
Zavis <thezavis@gmail.com>
2024-12-04 20:27:13 +1100

    Java V2 Seats V5 static insert seats to db and generateTimeSlots function

--- src/main/java/reservation/Service/SlotService.java
+++ src/main/java/reservation/Service/SlotService.java
@@ -1,5 +1,24 @@
 package reservation.Service;
 
-public class SlotService {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
+public class SlotService {
+	private final List<LocalTime> generateTimeSlots = generateTimeSlots();
+
+	private List<LocalTime> generateTimeSlots() {
+		List<LocalTime> timeSlots = new ArrayList<>();
+
+		LocalTime startTime = LocalTime.of(17, 30);
+		LocalTime endTime = LocalTime.of(21, 15);
+
+		while (startTime.isBefore(endTime)) {
+			timeSlots.add(startTime);
+			startTime = startTime.plusMinutes(15);
+		}
+
+		return timeSlots;
+	}
 }
---
+++ src/main/resources/db/migration/V5__Insert_seats_to_table_seats.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO seats (seat_name, capacity)
+VALUES ('Table 1', 2 ), ('Table 2', 2 ), ('Table 3', 2 ), ('Table 4', 2 );
+
+INSERT INTO seats (seat_name, capacity)
+VALUES ('Table 5', 4 ), ('Table 6', 4 ), ('Table 7', 4 ), ('Table 8', 4 );
+
+INSERT INTO seats (seat_name, capacity)
+VALUES ('Table 9', 6 ), ('Table 10', 6 ), ('Table 11', 6 ), ('Table 12', 6 );
