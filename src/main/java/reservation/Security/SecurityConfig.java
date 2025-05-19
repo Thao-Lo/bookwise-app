4c9e2e7350c390d9cbfbc6e0f5f446f602c58a43
zavis <thezavis@gmail.com>
2025-05-19 15:44:47 +1000

    Fix CORS with S3 endpoint and add aws s3 jdk to pom.xml

--- src/main/java/reservation/Security/SecurityConfig.java
+++ src/main/java/reservation/Security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedOrigins(Arrays.asList("http://my-bookwise-app.s3-website-ap-southeast-2.amazonaws.com", "http://localhost:3000"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 		configuration.setAllowCredentials(true);
