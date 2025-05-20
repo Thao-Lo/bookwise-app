0fd9aaea3ee90e7fc812364f5d929892d1f94021
zavis <thezavis@gmail.com>
2025-05-20 21:28:05 +1000

    Update LettuceCongig

--- src/main/java/reservation/Redis/LettuceConfig.java
+++ src/main/java/reservation/Redis/LettuceConfig.java
@@ -2,9 +2,11 @@ package reservation.Redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
+@Configuration
 public class LettuceConfig {
 	@Value("${spring.redis.host}")
 	private String hostName;
