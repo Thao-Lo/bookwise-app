e188c81bf41db6289548a31fe3740503bf872fcc
zavis <thezavis@gmail.com>
2025-05-22 01:21:30 +1000

    try to fix Reids LettuceConnectionFasctory not connect to host:redis, update RedisExpirationListener - V5

--- src/main/java/reservation/Redis/RedisConfig.java
+++ src/main/java/reservation/Redis/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -14,7 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 	 // must have return method for bean
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+	public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory) {
 
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 
@@ -37,9 +38,9 @@ public class RedisConfig {
 
 		return template;
 	}
-	
+	@DependsOn({"redisMessageListenerContainer", "lettuceConnectionFactory"})
 	@Bean
-	public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory) {
+	public RedisMessageListenerContainer redisMessageListenerContainer(LettuceConnectionFactory redisConnectionFactory) {
 		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 		container.setConnectionFactory(redisConnectionFactory);
 		return container;
