098d274f075cd8378edd020fafeaeeb56939174c
zavis <thezavis@gmail.com>
2025-05-22 02:27:59 +1000

    REDIS-LETTUCE persistent problem

--- src/main/java/reservation/Redis/RedisListenerConfig.java
+++ src/main/java/reservation/Redis/RedisListenerConfig.java
@@ -1,8 +1,15 @@
 package reservation.Redis;
 
+import reservation.Service.StripeService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+import reservation.Service.RedisService;
+import reservation.Service.SlotService;
+
 
 @Configuration
 @Profile("prod")
