bef103d208cd6891fc1194fd6911e4517aebdaea
zavis <thezavis@gmail.com>
2025-05-22 04:33:46 +1000

    retry redis.enable

--- src/main/java/reservation/Redis/RedisListenerConfig.java
+++ src/main/java/reservation/Redis/RedisListenerConfig.java
@@ -1,24 +1,24 @@
-package reservation.Redis;
-
-import reservation.Service.StripeService;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-
-import reservation.Service.RedisService;
-import reservation.Service.SlotService;
-
-
-@Configuration
-//@Profile("prod")
-public class RedisListenerConfig {
-    @Bean
-    public RedisExpirationListener redisExpirationListener(RedisMessageListenerContainer container,
-                                                           SlotService slotService,
-                                                           RedisService redisService,
-                                                           StripeService stripeService) {
-        return new RedisExpirationListener(container, slotService, redisService, stripeService);
-    }
-}
+//package reservation.Redis;
+//
+//import reservation.Service.StripeService;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+//
+//import reservation.Service.RedisService;
+//import reservation.Service.SlotService;
+//
+//
+//@Configuration
+////@Profile("prod")
+//public class RedisListenerConfig {
+//    @Bean
+//    public RedisExpirationListener redisExpirationListener(RedisMessageListenerContainer container,
+//                                                           SlotService slotService,
+//                                                           RedisService redisService,
+//                                                           StripeService stripeService) {
+//        return new RedisExpirationListener(container, slotService, redisService, stripeService);
+//    }
+//}
