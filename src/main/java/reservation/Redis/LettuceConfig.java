5c54c0590342761165a1dfb05aba40bd44f5d5d0
zavis <thezavis@gmail.com>
2025-05-22 01:52:54 +1000

    REDIS-LETTUCE persistent problem

--- src/main/java/reservation/Redis/LettuceConfig.java
+++ src/main/java/reservation/Redis/LettuceConfig.java
@@ -3,13 +3,14 @@ package reservation.Redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import jakarta.annotation.PostConstruct;
-
+@Primary
 @Configuration
 public class LettuceConfig {
 	@Value("${spring.redis.host}")
