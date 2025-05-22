021deaa7c3740e87591618111db85cde41ab59b5
zavis <thezavis@gmail.com>
2025-05-23 02:02:58 +1000

    Fix Spring not connect to Redis: redis

--- src/main/java/reservation/Application.java
+++ src/main/java/reservation/Application.java
@@ -6,11 +6,16 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import reservation.Redis.LettuceConfig;
+import reservation.Redis.RedisConfig;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(basePackages = {"reservation"})
+@Import(reservation.Redis.LettuceConfig.class)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
