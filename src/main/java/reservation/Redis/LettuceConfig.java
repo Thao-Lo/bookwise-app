e46b31fab672b7967247c2fb13ebf02d054ad0f1
zavis <thezavis@gmail.com>
2025-05-23 02:07:08 +1000

    Fix Spring not connect to Redis: redis

--- src/main/java/reservation/Redis/LettuceConfig.java
+++ src/main/java/reservation/Redis/LettuceConfig.java
@@ -1,16 +1,18 @@
 package reservation.Redis;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import jakarta.annotation.PostConstruct;
 
-@Configuration
-//@ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true", matchIfMissing = true)
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true", matchIfMissing = true)
 public class LettuceConfig {
 	@Value("${spring.redis.host}")
 	private String hostName;
@@ -20,18 +22,22 @@ public class LettuceConfig {
 	
 	@Value("${spring.profiles.active:dev}")
 	private String profile;
-	
+	static {
+	    System.out.println("🚨 LettuceConfig CLASS LOADED");
+	}
 	@PostConstruct
 	public void debugLettuceConfig() {
 	    System.out.println("Redis Host from @Value: " + hostName);
 	}
 	@Primary
-	@Bean(name = "customeLettuceConnectionFactory" )
-	public LettuceConnectionFactory customeLettuceConnectionFactory() {
+	@Bean(name = "customLettuceConnectionFactory" )
+	public LettuceConnectionFactory customLettuceConnectionFactory() {
 		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
 		config.setHostName(hostName);
 		config.setPort(port);
 		System.out.println("LETTUCE REDIS HOST??? " + hostName);
 		return new LettuceConnectionFactory(config);
 	}
+	
+	   
 }
