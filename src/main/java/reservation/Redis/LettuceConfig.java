b955d263eefd028cd336118bb59b32cf8a182c7b
zavis <thezavis@gmail.com>
2025-05-23 02:16:50 +1000

    Fix Spring not connect to Redis: redis

--- src/main/java/reservation/Redis/LettuceConfig.java
+++ src/main/java/reservation/Redis/LettuceConfig.java
@@ -14,28 +14,28 @@ import jakarta.annotation.PostConstruct;
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnProperty(name = "spring.redis.enabled", havingValue = "true", matchIfMissing = true)
 public class LettuceConfig {
-	@Value("${spring.redis.host}")
-	private String hostName;
-
-	@Value("${spring.redis.port}")
-	private int port;
-	
-	@Value("${spring.profiles.active:dev}")
-	private String profile;
+//	@Value("${spring.redis.host}")
+//	private String hostName;
+//
+//	@Value("${spring.redis.port}")
+//	private int port;
+//	
+//	@Value("${spring.profiles.active:dev}")
+//	private String profile;
 	static {
 	    System.out.println("🚨 LettuceConfig CLASS LOADED");
 	}
 	@PostConstruct
 	public void debugLettuceConfig() {
-	    System.out.println("Redis Host from @Value: " + hostName);
+	    System.out.println("Redis Host from @Value: redis");
 	}
 	@Primary
 	@Bean(name = "customLettuceConnectionFactory" )
 	public LettuceConnectionFactory customLettuceConnectionFactory() {
 		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-		config.setHostName(hostName);
-		config.setPort(port);
-		System.out.println("LETTUCE REDIS HOST??? " + hostName);
+		config.setHostName("redis");
+		config.setPort(6379);
+		System.out.println("LETTUCE REDIS HOST??? " );
 		return new LettuceConnectionFactory(config);
 	}
 	
