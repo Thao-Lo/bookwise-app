f165c0655a9f42b2ed4c13ca43b72c1b3e3097bc
zavis <thezavis@gmail.com>
2025-06-06 23:22:23 +1000

    OAuth2: Security Config with customOAuth2UserService and SuccessHandler

--- src/main/java/reservation/Security/SecurityConfig.java
+++ src/main/java/reservation/Security/SecurityConfig.java
@@ -15,24 +15,39 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import reservation.Auth.OAuth2.CustomOAuth2UserService;
+import reservation.Auth.OAuth2.OAuth2SuccessHandler;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Autowired
 	JwtAuthenticationFilter jwtAuthenticationFilter;
-
+	
+	@Autowired 
+	CustomOAuth2UserService customOAuth2UserService;
+	
+	@Autowired
+	OAuth2SuccessHandler oAuth2SuccessHandler;
+	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable()) // disable CSRF
 				// defining access control rules for different endpoints
 				.cors(Customizer.withDefaults()) // Enable CORS with default config
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests((requests) -> requests
 						.requestMatchers("/api/v1/user/profile").hasAnyRole("GUEST", "ADMIN")
 						.requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
 						.requestMatchers("/api/v1/user/**").hasRole("GUEST")						
 						.anyRequest().permitAll() // all other endpoints require authentication
-				).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				)
+				.oauth2Login(oauth -> oauth
+						.userInfoEndpoint(userInfor -> userInfor.userService(customOAuth2UserService))
+						.successHandler(oAuth2SuccessHandler)
+						)
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+				
 
 		return http.build();
 
