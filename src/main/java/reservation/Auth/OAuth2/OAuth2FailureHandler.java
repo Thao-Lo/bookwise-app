d8d41703d0c9c384f70eed58a545fd7cb3919992
zavis <thezavis@gmail.com>
2025-06-12 01:07:06 +1000

    OAUTH2: using Environment instead of Value for profiles.active

--- src/main/java/reservation/Auth/OAuth2/OAuth2FailureHandler.java
+++ src/main/java/reservation/Auth/OAuth2/OAuth2FailureHandler.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -16,12 +18,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Service
 public class OAuth2FailureHandler implements AuthenticationFailureHandler {
-	@Value("${spring.profiles.active}")
-	private String profile;
+	private final Environment env;
+	
+	public OAuth2FailureHandler(Environment env) {
+		this.env = env;
+	}
+	private final static Logger logger = LoggerFactory.getLogger(OAuth2FailureHandler.class);
 	
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException{
+		String profile = env.getProperty("spring.profiles.active", "dev");
+		logger.info("ACTIVE PROFILE" + profile);
+		
 		String protocol = profile.equalsIgnoreCase("prod") ? "https://zavism.com" : "http://localhost:3000";
 		//error: Email is used with .... -> after encode: Email+is+used+with, no white space on URL
 		String errorMessage = ((OAuth2AuthenticationException)exception).getError().getDescription();
--- src/main/java/reservation/Auth/OAuth2/OAuth2SuccessHandler.java
+++ src/main/java/reservation/Auth/OAuth2/OAuth2SuccessHandler.java
@@ -2,19 +2,15 @@ package reservation.Auth.OAuth2;
 
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Map;
-import java.util.Random;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,47 +26,47 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
 	@Autowired
 	JwtUtil jwtUtil;
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
+
+	private final Environment env;
+
+	public OAuth2SuccessHandler(Environment env) {
+		this.env = env;
+	}
 	
-	@Value("${spring.profiles.active}")
-	private String profile;
-	
-	@Override 
+	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication authentication) throws IOException, ServletException{
-		String protocol = profile.equalsIgnoreCase("prod") ? "https://zavism.com" : "http://localhost:3000";
+			Authentication authentication) throws IOException, ServletException {
+		String profile = env.getProperty("spring.profiles.active", "dev");
 		
-		//Get authenticated OAuth2User from security context
+		String protocol = profile.equalsIgnoreCase("prod") ? "https://zavism.com" : "http://localhost:3000";
+
+		// Get authenticated OAuth2User from security context
 		OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-		//Extract email from OAuth2 Provider: Google
+		// Extract email from OAuth2 Provider: Google
 		String email = oAuth2User.getAttribute("email");
 		System.out.println("OAUTH: INSIDE SUCCESS HANDLER");
-		//find user by email
+		// find user by email
 		User user = userService.findUserByEmail(email);
-		
-		//generate token, username == email
+
+		// generate token, username == email
 		String accessToken = jwtUtil.generateAccessToken(user.getUsername(), user.getRole().name());
 		String refreshToken = jwtUtil.generateRefreshToken(user.getUsername(), user.getRole().name());
-		LoginResponse loginResponse = new LoginResponse(
-				"Login successfully.",
-				accessToken,
-				refreshToken,
-				new UserResponse(user.getId(), user.getUsername(), user.getEmail(), user.getRole().name()
-				));			
+		LoginResponse loginResponse = new LoginResponse("Login successfully.", accessToken, refreshToken,
+				new UserResponse(user.getId(), user.getUsername(), user.getEmail(), user.getRole().name()));
 
-		
-		//Create sessionId and store with body in redis 
+		// Create sessionId and store with body in redis
 		String sessionId = UUID.randomUUID().toString();
 		redisTemplate.opsForValue().set("oauth2:sessionId:" + sessionId, loginResponse, Duration.ofMinutes(2));
-		
-		//write response back to client with sessionId + status 302:FOUND
-		response.sendRedirect(protocol + "/oauth2/success?oauth2sessionId=" + sessionId);		
-		//localhost:8080/oauth2/authorization/google
+
+		// write response back to client with sessionId + status 302:FOUND
+		response.sendRedirect(protocol + "/oauth2/success?oauth2sessionId=" + sessionId);
+		// localhost:8080/oauth2/authorization/google
 	}
 
 }
